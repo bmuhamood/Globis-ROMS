1352a9ec90d6c86df7bdaa5e996721f59962fcb0
Bbosa Muhamood <bbosa2009@gmail.com>
2026-02-21 23:30:10 -0800

    Add database

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -10,6 +10,3 @@ class AccountsConfig(AppConfig):
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-
-        if username and password and not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
